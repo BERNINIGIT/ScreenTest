22f7faf46f1b076675ec7bd3326180938ba96320
zurisadai <zurisadai@LAPTOP-B8OPCG5E>
2022-04-05 18:16:17 -0500

    Agregar archivos de proyecto.

---
+++ ScreenTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ScreenTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FoodContext _foodContext;
+
+        public HomeController(ILogger<HomeController> logger, FoodContext foodContext)
+        {
+            _logger = logger;
+            _foodContext = foodContext;
+        }
+        //[HttpPost]
+        public IActionResult Index(string customCase = "NoCasing")
+        {
+            //List<string> dishes = new List<string>() { "Dish1", "Dish2"};
+            List<string> dishes = _foodContext.Dishes.Select( s => s.DishName).ToList();
+            if(customCase == "UpperCase")
+                dishes = dishes.Select(x => x.ToUpperInvariant()).ToList();
+            else if(customCase == "LowerCase")
+                dishes = dishes.Select(x => x.ToLowerInvariant()).ToList();
+            return View(dishes);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ScreenTest/Models/FoodDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScreenTest.Models
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+        public string DishName { get; set; }        
+    }
+
+    public class FoodContext : DbContext
+    {
+        public FoodContext(DbContextOptions options)
+            : base(options)
+        {
+            //If model change, It will re-create new database.
+            //Database.SetInitializer<personContext>(new DropCreateDatabaseIfModelChanges<personContext>());
+        }
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
